0e16fe4312c0a1bda967f74b231dab93a9718666
Naviabheeman <navia@chaintope.com>
2025-02-26 15:18:33 +0530

    boost hedaers in link

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -5,6 +5,6 @@ $(package)_file_name=boost_$(subst .,_,$($(package)_version)).tar.gz
 $(package)_sha256_hash=205666dea9f6a7cfed87c7a6dfbeb52a2c1b9de55712c9c1a87735d7181452b6
 
 define $(package)_stage_cmds
-  mkdir -p $($(package)_staging_prefix_dir)/include && \
-  cp -r boost $($(package)_staging_prefix_dir)/include
+	mkdir -p $($(package)_staging_prefix_dir)/include && \
+	cp -r boost $($(package)_staging_prefix_dir)/include
 endef
--- src/random.cpp
+++ src/random.cpp
@@ -20,6 +20,7 @@
 #include <stdlib.h>
 #include <chrono>
 #include <thread>
+#include <unistd.h>
 
 #include <randomenv.h>
 
